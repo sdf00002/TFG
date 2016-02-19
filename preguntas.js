d2f2824ae8b823477f80c126341d64609250ebeb
sdf00002 <sdf00002@red.ujaen.es>
2016-02-19 11:07:02 +0100

    Cambios importantes
    Se ha conseguido la aleatoriedad en las preguntas en todos los
    navegadores así como de sus opciones. De momento, es obligatorio
    seleccionar una opción, no se descarta eliminar esa posibilidad. Se ha
    cambiado el tipo del botón validar de submit a button para evitar que se
    recargue la página una vez se pulsen.
    También se ha añadido en preguntas.js la función arrayAlea() que nos
    permite obtener un array con valores aleatorios dependiendo de la
    longitud del array que le pasemos como parámetro.

--- preguntas.js
+++ preguntas.js
@@ -1,6 +1,7 @@
 // JavaScript Document
 // Nombre del archivo: preguntas.js
 
+//Variable para puntuacion
 
 function cargarXMLDoc(archivoXML) 
 {
@@ -25,6 +26,73 @@ function cargarXMLDoc(archivoXML)
  return null;
 }
 
+function loadXMLDoc() {
+  var xmlhttp;
+  if (window.XMLHttpRequest) {
+    xmlhttp = new XMLHttpRequest();
+  } else {
+    // code for older browsers
+    xmlhttp = new ActiveXObject("Microsoft.XMLDOM");
+  }
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      myFunction(xmlhttp);
+    }
+  };
+  xmlhttp.open("GET", "preguntas.xml", true);
+  xmlhttp.send();
+}
+
+function myFunction(xml) {
+    var xmlDoc = xml.responseXML;
+	if (xmlDoc != null)
+{
+	// En la variable div_preguntas obtenemos el contenedor div con el id 'preguntas'
+	var div_preguntas = document.getElementById('preguntas');
+
+ // Obtenemos la lista de preguntas
+	var preguntas_tag = xmlDoc.getElementsByTagName("preguntas")[0].getElementsByTagName("pregunta");
+	
+	//Mezclamos un array auxiliar que nos servirá para obtener las preguntas de forma aleatoria
+	var arrayaux=arrayAlea(preguntas_tag);
+	for (var i = 0; i < 4; i++)
+ {
+  // Obtenemos el título de la pregunta
+  var titulo = preguntas_tag[arrayaux[i]].getElementsByTagName("titulo")[0].childNodes[0].nodeValue;
+  //Obtenemos el tipo de la pregunta
+  var tipo=preguntas_tag[arrayaux[i]].attributes[0].nodeValue;
+
+  // Obtenemos las opciones de la pregunta
+  var opciones_tag = preguntas_tag[arrayaux[i]].getElementsByTagName("opcion");
+  
+  var opciones="";
+
+	if(tipo=="unica")
+		opciones+=unica(opciones_tag,i);
+		else
+			if(tipo=="multiple")
+			opciones+=multiple(opciones_tag,i);
+				else
+					opciones+=numerica(opciones_tag,i);
+
+  // Modificamos el contenido html del contenedor div
+   div_preguntas.innerHTML += "<p>" + titulo + "</p>"+opciones;
+	
+ }
+
+}
+}
+
+//Funcion para obtener un array aleatorio dependiendo de la longitud del array de entrada
+function arrayAlea(array){
+	var unArray=[];
+	for(var z=0;z<array.length;z++){
+		unArray[z]=z;
+	}
+	fisher_yates(unArray);
+	return unArray;
+}
+
 /*
  * Aleatoriza un array según el algoritmo de Fisher-Yates
  */
@@ -38,44 +106,39 @@ function fisher_yates(array){
     }
 }
 //Funcion crear pregunta respuesta unica
-function unica(opciones_tag){
+function unica(opciones_tag,ent){
 	var iden=opciones_tag[0].getAttribute("name");
-	var opciones="<form name=\"formulario\" action=\"#\" onsubmit=\"return validacion("+iden+")\" method=\"post\">";
+	var vector=arrayAlea(opciones_tag);
+	var opciones="<form name=\"formulario\">";
 	for (var j = 0; j < opciones_tag.length; j++){
-	opciones += "<input type=\"radio\""+atributos(opciones_tag[j])+"/>"+opciones_tag[j].childNodes[0].nodeValue+"</br>";
+	opciones += "<input type=\"radio\""+atributos(opciones_tag[vector[j]])+"/>"+opciones_tag[vector[j]].childNodes[0].nodeValue+"</br>";
 	}
-		opciones += "<input type=\"submit\" value=\"Validar\"/><input type=\"reset\" value=\"Borrar\" /></form>";
+		opciones += "<input name=\"valida"+ent+"\" type=\"button\" value=\"Validar\" onclick=\"puntuar("+iden+","+ent+")\"/><input type=\"reset\" value=\"Borrar\" /><form>";
 	return opciones;
 	
 }
 
-//Funcion crear pregunta respuesta unica2
-function unica2(opciones_tag){
-	var opciones="<form name=\"formulario\" action=\"aplicacion/app\" onsubmit=\"return validacion(this)\" method=\"post\">";
-	for (var j = 0; j < opciones_tag.length; j++){
-	opciones += "<input type=\"radio\" name=\"equipo\"/>"+opciones_tag[j]+"</br>";
-	}
-		opciones += "<input type=\"submit\" value=\"Enviar respuesta\"/><input type=\"reset\" value=\"Borrar\" /></form>";
-	return opciones;
-}
 
 //Funcion crear pregunta respuesta multiple
-function multiple(opciones_tag){
-	var opciones="<form name=\"formulario\" action=\"#\" onsubmit=\"return validacion("+opciones_tag[0].getAttribute("name")+")\" method=\"post\">";
+function multiple(opciones_tag,ent){
+	var iden=opciones_tag[0].getAttribute("name");
+	var vector=arrayAlea(opciones_tag);
+	var opciones="<form name=\"formulario\">";
 	for (var j = 0; j < opciones_tag.length; j++){
-	opciones += "<input type=\"checkbox\""+atributos(opciones_tag[j])+"/>"+opciones_tag[j].childNodes[0].nodeValue+"</br>";
+	opciones += "<input type=\"checkbox\""+atributos(opciones_tag[vector[j]])+"/>"+opciones_tag[vector[j]].childNodes[0].nodeValue+"</br>";
 	}
-		opciones += "<input type=\"submit\" value=\"Validar\"/><input type=\"reset\" value=\"Borrar\" /></form>";
+		opciones += "<input name=\"valida"+ent+"\" type=\"button\" value=\"Validar\" onclick=\"puntuar("+iden+","+ent+")\"/><input type=\"reset\" value=\"Borrar\" /></form>";
 	return opciones;
 }
 
-//Funcion crear pregunta respuesta multiple2
-function multiple2(opciones_tag){
-	var opciones="<form name=\"formulario\" action=\"aplicacion/app\" onsubmit=\"return validacion(this)\" method=\"post\">";
-	for (var j = 0; j < opciones_tag.length; j++){
-	opciones += "<input type=\"checkbox\" name=\"equipo\"/>"+opciones_tag[j]+"</br>";
-	}
-		opciones += "<input type=\"submit\" value=\"Enviar respuesta\"/><input type=\"reset\" value=\"Borrar\" /></form>";
+//Funcion crear pregunta numerica
+function numerica(opciones_tag,ent){
+	var iden=opciones_tag[0].getAttribute("name");
+	var inferior=parseFloat(opciones_tag[0].getAttribute("value"));
+	var superior=parseFloat(opciones_tag[1].getAttribute("value"));
+	var opciones="<form name=\"formulario\" onsubmit=\"return puntuar("+iden+","+ent+","+inferior+","+superior+")\">";
+	opciones += "Escribe el valor: <input type=\"text\" name=\""+opciones_tag[0].getAttribute("name")+"\" size=\"3\" maxlength=\"3\"/></br></br>";
+		opciones += "<input name=\"valida"+ent+"\" type=\"submit\" value=\"Validar\"/><input type=\"reset\" value=\"Borrar\" /></form>";
 	return opciones;
 }
 
@@ -96,35 +159,15 @@ function array_opciones(opciones_tag){
 	}
 	return array;
 }
-/*
-//Funcion para validar el formulario
-function validacion(F,nombre){
+
+//Funcion para validar el formulario auxiliar
+function validacion2(iden){
  var i;
  var ok=0;
-
- for(i=0; i<F.nombre.length;i++){
-  if(F.nombre[i].checked)
-  {
-   ok=1
-  }    
- }
- 
- if(ok==1)
-  return true
-	else{
-		alert("Debes seleccionar una opcion");
-	return false	}
-}
-*/
-//Funcion para validar el formulario
-function validacion(iden){
- var i;
- var ok=0;
-var opt=document.getElementsByName(iden[0].name);
-
+//var opt=document.getElementsByName(iden[0].name);
+var opt=document.getElementsByName(iden);
  for(i=0; i<opt.length;i++){
- // if(opt[i].checked)
-	if(opt[i].checked && opt[i].getAttribute("correcta")=="true")
+	if(opt[i].checked)
   {
    ok=1
   // alert(opt[i].attributes[2].value);
@@ -135,6 +178,113 @@ var opt=document.getElementsByName(iden[0].name);
  if(ok==1)
   return true
 	else{
-		alert("Debes seleccionar una opcion");
+		//alert("Debes seleccionar una opcion");
 	return false	}
+}
+
+
+//Funcion para validar el formulario
+function validacion(iden){
+ var i;
+ var ok=0;
+var opt=document.getElementsByName(iden[0].name);
+//var opt=document.getElementsByName(iden);
+ for(i=0; i<opt.length;i++){
+	if(opt[i].checked)
+  {
+   ok=1
+  // alert(opt[i].attributes[2].value);
+  //alert(opt[i].getAttribute("correcta"));
+  }    
+ }
+
+ if(ok==1)
+  return true
+	else{
+		//alert("Debes seleccionar una opcion");
+	return false	}
+}
+
+//Funcion para validar un campo de texto
+function validaTexto(iden){
+var opt=iden.value;
+if( opt == null || opt.length == 0)
+	return false;
+else 
+	return true;
+}
+
+//Funcion para puntuar una pregunta unica o multiple
+function puntuar(iden, ent){
+	var puntuacion=0;
+	var validado=validacion(iden);
+		if (validado==false){
+			alert("Debes seleccionar una opción");
+			return false;
+		}
+		else {
+			var opt=document.getElementsByName(iden[0].name);
+			var ok=0;
+			for(var i=0; i<opt.length;i++){
+				if(opt[i].checked && opt[i].getAttribute("puntos")!="0"){
+					puntuacion+=parseFloat(opt[i].getAttribute("puntos"));
+					document.getElementsByName("valida"+ent).disabled = true; 
+				}
+		}
+		document.getElementsByName("valida"+ent)[0].disabled = true;
+		alert("Tu puntuación es de: "+puntuacion);
+		return true;
+	}
+}
+
+//Funcion para puntuar una pregunta numerica
+/*
+function puntuar(iden,ent,inf,sup){
+	var puntuacion=0;
+	var validado=validaTexto(iden);
+	if (validado==false){
+			alert("Debes incluir un valor");
+			return false;
+		}
+			else {
+			var valor=parseFloat(iden.value);
+			if(valor<=sup && valor>=inf)
+				puntuacion+=5;
+		document.getElementsByName("valida"+ent)[0].disabled = true;
+		alert("Tu puntuación es de: "+puntuacion);
+		return true;
+	}
+}
+*/
+//Funcion para corregir todas las preguntas a la vez
+function corrige(){
+	var puntuacion=0;
+	// Obtenemos las opciones de la pregunta
+  var opciones_tag = document.getElementsByTagName("input");
+
+	for(var i=0; i<opciones_tag.length-1;i=i+6){	
+	//if(opciones_tag[i].type=="radio" || opciones_tag[i].type=="checkbox" || opciones_tag[i].type=="text"){
+  	var nombre=opciones_tag[i].getAttribute("name");
+
+	var seleccionado=validacion2(nombre);
+	
+		if(seleccionado==false){
+			alert("Hay alguna pregunta sin responder");
+			return false;
+		}
+		else {
+			var opt=document.getElementsByName(nombre);
+			var ok=0;
+			for(var j=0; j<opt.length;j++){
+				if(opt[j].checked && opt[j].getAttribute("puntos")!="0"){
+					puntuacion+=parseFloat(opt[j].getAttribute("puntos"));
+				}
+		}		
+	}
+	//}
+	}
+	alert("Tu puntuación es de: "+puntuacion);
+	document.getElementsByName("test")[0].disabled = true;
+	return true;
+		
 }
